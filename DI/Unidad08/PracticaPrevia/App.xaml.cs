22aea97d822a4b2f573e8123f70b326149ca34e0
BaiHuu <alvaro.salvador@iesnervion.es>
2024-10-16 14:33:40 +0200

    Creacion de solución

---
+++ DI/Unidad08/PracticaPrevia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticaPrevia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
