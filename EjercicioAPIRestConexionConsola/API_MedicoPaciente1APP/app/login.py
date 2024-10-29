16732e6f0e3e8eda6c5b262109ffceef4b075b7d
BaiHuu <alvaro.salvador@iesnervion.es>
2024-10-29 12:45:55 +0100

    Actualización, vuelta a subir, TERMINAR INTERFAZ Y FUNCIONALIDAD

---
+++ EjercicioAPIRestConexionConsola/API_MedicoPaciente1APP/app/login.py
@@ -0,0 +1,16 @@
+import token
+import requests
+
+
+def login():
+    username = input("User: ")
+    password = input("Password: ")
+    resultado = requests.post(
+        "http://localhost:5050/Users/login",
+        json ={"username": username, "password": password},
+        headers ={"Content-Type": "application/json"}
+        token = resultado.json().get("token"))
+    return token
+
+def registro():
+        return False
