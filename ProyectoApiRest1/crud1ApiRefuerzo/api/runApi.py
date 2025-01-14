797ef6bd0c9eabad05491a4a05c8bbd2e1579ac5
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-15 00:29:22 +0100

    verbos hechos y api rudimentaria.

--- ProyectoApiRest1/crud1ApiRefuerzo/api/runApi.py
+++ ProyectoApiRest1/crud1ApiRefuerzo/api/runApi.py
@@ -57,9 +57,9 @@ def get_peliculas():
 
 @app.get("/peliculas/<int:id>")
 def get_peliculasPorID(id):
-    for contenido in peliculas:
-        if contenido["Id"] == id: #? La 'clave' de Clave valor debe corresponder al 100% con la clave que tenemos en la BD o diccionario
-            return contenido, 200
+    for peli in peliculas:
+        if peli["Id"] == id: #? La 'clave' de Clave valor debe corresponder al 100% con la clave que tenemos en la BD o diccionario
+            return peli, 200
         else: 
             return {"error": "Pelicula no encontrada"}, 404
 
@@ -72,14 +72,52 @@ def get_series():
 
 @app.get("/series/<int:id>")
 def get_seriesPorID(id):
-    for contenido in series:
-        if contenido["Id"] == id: #? La 'clave' de Clave valor debe corresponder al 100% con la clave que tenemos en la BD o diccionario
-            return contenido, 200
+    for peli in series:
+        if peli["Id"] == id: #? La 'clave' de Clave valor debe corresponder al 100% con la clave que tenemos en la BD o diccionario
+            return peli, 200
         else: 
             return {"error": "Serie no encontrada"}, 404
         
         
-#*PUT/PATCH-------------------------------------------------------------------------------------------        
+#*POST.PELICULAS--------------------------------------------------------------------------------------
+# Crearemos una funcion que calcule el siguiente ID. Cogiendo el maximo id que encuentre en la lista y le suma +1
+def idAutoincrementadoPeliculas():
+    return max(peli["Id"] for peli in peliculas) + 1
+
+@app.post("/peliculas")
+def add_pelicula():
+    # Se comprueba si la petición que nos ha llegado cumple con el formato JSON
+    if request.is_json:
+        # Creamos una variable donde guardamos el formato JSON
+        peli = request.get_json()
+        # Le indicamos al diccionario country que su nuevo ID es el que nos devuelve la función _find_next_id()
+        peli["Id"] = idAutoincrementadoPeliculas()
+        # Añadimos la nueva pelicula
+        peliculas.append(peli)
+        # Devolve la pelicula en formato diccionario y el codigo 201 para indicar que se ha añadido
+        return peli, 201
+    return{"error":"Request must be JSON"}, 415
+
+#*POST.SERIES--------------------------------------------------------------------------------------
+# Crearemos una funcion que calcule el siguiente ID. Cogiendo el maximo id que encuentre en la lista y le suma +1
+def idAutoincrementadoPeliculas():
+    return max(serie["Id"] for serie in series) + 1
+
+@app.post("/series")
+def add_pelicula():
+    # Se comprueba si la petición que nos ha llegado cumple con el formato JSON
+    if request.is_json:
+        # Creamos una variable donde guardamos el formato JSON
+        serie = request.get_json()
+        # Le indicamos al diccionario country que su nuevo ID es el que nos devuelve la función _find_next_id()
+        serie["Id"] = idAutoincrementadoPeliculas()
+        # Añadimos la nueva pelicula
+        peliculas.append(serie)
+        # Devolve la pelicula en formato diccionario y el codigo 201 para indicar que se ha añadido
+        return serie, 201
+    return{"error":"Request must be JSON"}, 415
+
+#*PUT/PATCH--------------------------------------------------------------------------------------       
 # Ahora haremos el PUT/PATCH que es lo mismo practicamente: 
 # Como tenemos que modificar una pelicula en concreto.
 @app.put("/peliculas/<int:id>")
@@ -91,14 +129,29 @@ def modificarPelicula(id):
         # Creamos una variable donde guardamos el formato JSON, que coincide con un DICCIONARIO
         nuevosDatos = request.get_json()
         # Tenemos que coger de la lista de peliculas, una pelicula en concreto seleccionandola por su 'Id'
-        for contenido in peliculas:
-            if contenido["Id"] == id:
+        for peli in peliculas:
+            if peli["Id"] == id:
                 # Modifucamos todos los atributos del país con los nuevos valores indicados en el JSON
                 for clave in nuevosDatos:
-                    contenido[clave] == nuevosDatos[clave]
+                    peli[clave] == nuevosDatos[clave]
                     # Devolvemos la película en formato diccionario y el codigo 200 de correcto
-                return contenido, 200
+                return peli, 200
     return {"error": "El formato tiene que ser JSON"}, 415
+
+#*DELETE--------------------------------------------------------------------------------------
+
+# A la dirección de peliculas o series se le añade la posibilidad de añadir un ID para eliminar la peli o serie a eliminar
+@app.delete("/peliculas/<int:id>")
+# Como hay que eliminar una pelicula en cuestion, tenemos que pasar el ID para eliminarlo, pues lo pasaremos por parametros
+def eliminar_pelicula(id):
+    # Como hay que eliminar una pelicula en concreto, tendremos que buscar en la lista el id de la pelicula que se ha indicado en la petición
+    for peli in peliculas:
+        if peli["Id"] == peliculas["Id"]:
+            peliculas.remove(peli)
+            return "{}", 200 # Si se encuentra el pais:
+    return {"Error":"Pelicula no encontrada"}, 404 # Si no se encuentra: 
+
+
 #*--------------------------------------------------------------------------------------------
 # Ahora Creamos nuestro 'main' y ahi ejecutamos nuestra API, en el puerto '5050' y en el host '0.0.0.0' 
 # Ya no hace falta tocar más el main.
