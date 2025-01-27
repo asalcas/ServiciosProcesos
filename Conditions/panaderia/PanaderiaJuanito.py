fec79739b1eb48ee1ab75cdd224089cc105092a9
BaiHuu <alvaro.salvador@iesnervion.es>
2025-01-27 10:21:19 +0100

    no entiendo

--- Conditions/panaderia/PanaderiaJuanito.py
+++ Conditions/panaderia/PanaderiaJuanito.py
@@ -10,10 +10,4 @@ class Panaderia(Thread):
     def __init__(self, nombre):
         Thread.__init__(self, name=nombre)
         
-    def gestionar_tienda(self):
-        with Panaderia.cond:
-            while Panaderia.panes_tiene > 0:
-                print(f"El cliente, {self.name} está esperando para comprar el pan")
-                Panaderia.cond.wait()
-
-            Panaderia.
+    
--- Conditions/panaderia/main.py
+++ Conditions/panaderia/main.py
@@ -1,7 +1,24 @@
 from threading import Thread, Condition
 import time
 import random
+from panaderia.PanaderiaJuanito import *
+from panaderia.Compradores import *
+from panaderia.Reponedores import *
 
 
 if __name__ == "__main__":
-    pass
+    NUM_COMPRADORES = 50 # Primero determinamos que cantidad de clientes tendremos en la "Panadería"
+    panificadoraSevilla = Panaderia() # Abrimos una nueva panaderia con el nombre "panificadoraSevilla"
+    lista = [] # Aquí es donde guardaremos la lista de personajes que van a interactuar en nuestra panadería: "Compradores" y
+    # "Reponedores"
+    
+    for i in range(NUM_COMPRADORES):
+        lista.append(Compradores(i,panificadoraSevilla, ))
+
+    lista.append(Reponedores(panificadoraSevilla, )) # Contratamos un reponedor de panes en la tienda
+
+    for c in lista:
+        c.start()
+
+    for c in lista: 
+        c.join()
